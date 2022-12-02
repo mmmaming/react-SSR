f2bbf12370928de755d820e532557ebfef9d4cfb
mmmaming <864845434@qq.com>
2022-12-02 11:48:33 +0800

    3.增加React并通过后端渲染

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 import express from 'express';
-
+import React from 'react';
+import { renderToString } from "react-dom/server";
+import HomePage from "../container/HomePage";
 const app = express();
 
-app.get('/', (req,res) => res.send(`
-<html>
-   <head>
-       <title>React Server Side Render</title>
-   </head>
-   <body>
-       Hello world
-   </body>
-</html>
-`));
+app.get('/', (req, res) => {
+    const content = renderToString(<HomePage />);
+    res.send(`
+        <html>
+           <head>
+               <title>React Server Side Render</title>
+           </head>
+           <body>
+               ${content}
+           </body>
+        </html>
+`)
+});
 
 app.listen(3000, () => console.log('listening on port 3000!'))
