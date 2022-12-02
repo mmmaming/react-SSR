d006e35a4a56a3476230c551edcb5ecf2135daa6
mmmaming <864845434@qq.com>
2022-12-02 16:54:00 +0800

    8.增加服务端路由

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import React from 'react';
 import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom/server";
 import HomePage from "../container/HomePage";
+import routes from "../routes";
 
 const app = express();
 
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-    const content = renderToString(<HomePage />);
+    const content = renderToString(
+        <StaticRouter location={req.path}>
+            {routes}
+        </StaticRouter>
+    );
+
     res.send(`
         <html>
            <head>
