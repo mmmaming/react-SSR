765af43635e9fd90697e771b35af31ba374b35b9
mmmaming <864845434@qq.com>
2022-12-04 10:48:08 +0800

    11.增加服务端发请求并渲染

--- container/HomePage.js
+++ container/HomePage.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const HomePage = () => {
+    const [ state, setState ] = useState(null);
     const handleClick = () => {
         console.log('oh, you clicked me');
     }
 
+
+    useEffect(() => {
+        axios.get("https://api.apiopen.top/api/getHaoKanVideo?page=0&size=2").then(res => {
+            setState(res?.data?.result?.list)
+        })
+    }, []);
     return (
         <div>
             <h2>
@@ -15,6 +23,15 @@ const HomePage = () => {
             <div>
                 <Link to="/park">Go to Park</Link>
             </div>
+            <h3>
+                here is some video
+            </h3>
+            <ul>
+                {
+                    state && state.map(item => <li key={item.id}>{item.title}</li>)
+                }
+            </ul>
+
         </div>
     )
 };
